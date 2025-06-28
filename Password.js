b3d2d2c8970201485752e7d605e0522050ac78a1
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-06-28 17:51:59 +0530

    update

---
+++ Password.js
@@ -0,0 +1,9 @@
+let main = document.querySelectorAll(".main");
+let Password = document.querySelector('#password');
+let button = document.querySelector("#generateBtn");
+let range = document.querySelector("#range");
+
+
+button.addEventListener("click", (e) => {
+    e.target.value
+})
