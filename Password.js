c8f880edb06c09f5fba55558a28cf1d196bec591
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-06-28 18:03:39 +0530

    update

--- Password.js
+++ Password.js
@@ -1,9 +1,18 @@
-let main = document.querySelectorAll(".main");
-let Password = document.querySelector('#password');
+let Password = document.querySelector("#password");
 let button = document.querySelector("#generateBtn");
 let range = document.querySelector("#range");
 
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-button.addEventListener("click", (e) => {
-    e.target.value
-})
+button.addEventListener("click", () => {
+  let Passwordlength = range.value;
+  let Generatepasswrod = "";
+
+  for (let i = 0; i < Passwordlength; i++) {
+    let random = Math.floor(Math.random() * characters.length);
+    Generatepasswrod += characters[random];
+  }
+
+  Password.value = Generatepasswrod;
+});
